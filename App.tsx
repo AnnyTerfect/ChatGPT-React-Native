f33b6978fc977eee4d6be7d14d7a7ace5dd19e2b
AnnyTerfect <1247470246@qq.com>
2023-05-16 21:27:57 +0800

    style: white primary color in dark theme

--- App.tsx
+++ App.tsx
@@ -6,17 +6,36 @@
  */
 
 import React from 'react';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {
+  Provider as PaperProvider,
+  MD3LightTheme,
+  MD3DarkTheme,
+} from 'react-native-paper';
 import MyApp from './src/MyApp';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useColorScheme } from 'react-native';
 
 export default function Main() {
+  const colorScheme = useColorScheme();
+
+  const theme =
+    colorScheme === 'dark'
+      ? {
+          ...MD3DarkTheme,
+          colors: { ...MD3DarkTheme.colors, primary: '#FFF' },
+        }
+      : {
+          ...MD3LightTheme,
+          colors: { ...MD3LightTheme.colors },
+        };
+
   return (
     <PaperProvider
       settings={{
         // eslint-disable-next-line react/no-unstable-nested-components
         icon: props => <MaterialCommunityIcons {...props} />,
-      }}>
+      }}
+      theme={theme}>
       <MyApp />
     </PaperProvider>
   );
