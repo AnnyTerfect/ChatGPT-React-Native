c2629b6c16e4516c15a8c4bb89671791fcfe45ba
AnnyTerfect <1247470246@qq.com>
2023-05-16 13:23:17 +0800

    style: adjust light mode

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -5,11 +5,10 @@ import { Appbar } from 'react-native-paper';
 export default function MyAppbar(props) {
   return (
     <Appbar.Header style={styles.header}>
-      <Appbar.Action icon="menu" color="white" onPress={props.onPressMenu} />
-      <Appbar.Content title="ChatGPT" color="white" />
+      <Appbar.Action icon="menu" onPress={props.onPressMenu} />
+      <Appbar.Content title="ChatGPT" />
       <Appbar.Action
         icon="dots-vertical"
-        color="white"
         onPress={props.onPressDots}
       />
     </Appbar.Header>
@@ -17,5 +16,5 @@ export default function MyAppbar(props) {
 }
 
 const styles = StyleSheet.create({
-  header: { backgroundColor: 'rgb(98, 0, 238)' },
+  header: {},
 });
